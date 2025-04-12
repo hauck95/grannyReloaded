55f73cfaee9c11f596ced99058ca7775ba56695e
unknown <hauck95@gmail.com>
2025-04-12 20:30:48 +0200

    Add controllable car mechanics basics

--- Scripts/vehicle.gd
+++ Scripts/vehicle.gd
@@ -1,1 +1,5 @@
 extends VehicleBody3D
+
+func _physics_process(delta: float) -> void:
+	engine_force = Input.get_axis("move_backwards", "move_forwards") * 150
+	steering = Input.get_axis("move_right", "move_left") * 0.4
